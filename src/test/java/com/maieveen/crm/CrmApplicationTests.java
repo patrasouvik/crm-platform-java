674823681b7f705fe8918f54ec7317479f92cf79
Souvik Kumar Patra <63934492+patrasouvik@users.noreply.github.com>
2026-09-12 16:18:46 +1000

    refactor: move tests to maieveen package

---
+++ src/test/java/com/maieveen/crm/CrmApplicationTests.java
@@ -0,0 +1,11 @@
+package com.maieveen.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
