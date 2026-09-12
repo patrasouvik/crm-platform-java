409046c6b5b526165c54518eff254dbf1194cb8b
Souvik Kumar Patra <63934492+patrasouvik@users.noreply.github.com>
2026-09-12 16:13:25 +1000

    test: add Spring context test

---
+++ src/test/java/com/skp/crm/CrmApplicationTests.java
@@ -0,0 +1,11 @@
+package com.skp.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
