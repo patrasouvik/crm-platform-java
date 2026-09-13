a9f8cbedcad5c87cf8edfe20509d2810335cad27
Souvik Kumar Patra <63934492+patrasouvik@users.noreply.github.com>
2026-09-13 16:53:18 +1000

    Add admin user repository

---
+++ src/main/java/com/maieveen/crm/security/AdminUserRepository.java
@@ -0,0 +1,12 @@
+package com.maieveen.crm.security;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
+
+    @EntityGraph(attributePaths = "roles")
+    Optional<AdminUser> findByUsername(String username);
+}
