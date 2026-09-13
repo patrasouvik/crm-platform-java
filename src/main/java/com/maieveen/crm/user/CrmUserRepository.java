d8b203735ad70d8210156d6c86a6b49565510569
Souvik Kumar Patra <63934492+patrasouvik@users.noreply.github.com>
2026-09-13 16:09:20 +1000

    Add CRM user duplicate lookup methods

--- src/main/java/com/maieveen/crm/user/CrmUserRepository.java
+++ src/main/java/com/maieveen/crm/user/CrmUserRepository.java
@@ -8,6 +8,14 @@ import java.util.List;
 
 public interface CrmUserRepository extends JpaRepository<CrmUser, Long> {
 
+    boolean existsByEmailIgnoreCase(String email);
+
+    boolean existsByPhone(String phone);
+
+    boolean existsByEmailIgnoreCaseAndIdNot(String email, Long id);
+
+    boolean existsByPhoneAndIdNot(String phone, Long id);
+
     @Query("select u from CrmUser u where lower(u.firstName) like lower(concat('%', :q, '%')) " +
            "or lower(u.lastName) like lower(concat('%', :q, '%')) " +
            "or lower(u.email) like lower(concat('%', :q, '%')) " +
